cd91ff7b89a6b28ad0b2f97337585115f92e0ab9
Bill <ceriustech@gmail.com>
2020-06-02 23:51:52 -0500

    Changed the way props are passed down in menu-item component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 
@@ -14,33 +14,14 @@ const HatsPage = (props) => {
   )
 }
 
-const ShoesPage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Shoes Page</h1>
-    </div>
-  )
-}
 
-const ShoesDetail = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>This is shoe item: {props.match.params.shoeId}</h1>
-    </div>
-  )
-}
+
 
 function App() {
   return (
     <div>
-      <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact  path='/hats' component={HatsPage} />
-        <Route exact  path='/shoes' component={ShoesPage} />
-        <Route exact  path='/shoes/:shoeId' component={ShoesDetail} />
-      </Switch>
+        <Route path='/hats' component={HatsPage} />
     </div>
   );
 }
