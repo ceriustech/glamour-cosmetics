1eee552905f83fdb48abfa659620afdcba8a3b4a
Bill <ceriustech@gmail.com>
2020-06-09 17:08:24 -0500

    Added preview-collections component.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,9 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'; 
 
 import './App.css';
-
-const HatsPage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  )
-}
 
 
 
@@ -21,7 +13,7 @@ function App() {
   return (
     <div>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
     </div>
   );
 }
