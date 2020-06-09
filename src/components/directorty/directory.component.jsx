38189b84fba889a4446c123c6c21f71135e58696
Bill <ceriustech@gmail.com>
2020-06-08 21:11:08 -0500

    Re-ordered state in the directory component and added the shop page.

--- src/components/directorty/directory.component.jsx
+++ src/components/directorty/directory.component.jsx
@@ -8,35 +8,45 @@ class Directory extends React.Component {
       state = {
         sections: [
           {
+            id: 1,
             title: 'hats',
             imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-            id: 1,
+            size: 'large',
             linkURL: 'hats'
           },
           {
+            id: 2,
             title: 'jackets',
             imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-            id: 2,
+            size: 'large',
             linkURL: ''
           },
           {
+            id: 3,
             title: 'sneakers',
             imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-            id: 3,
+            size: 'large',
             linkURL: ''
           },
           {
+            id: 4,
             title: 'womens',
             imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
             size: 'large',
-            id: 4,
             linkURL: ''
           },
           {
+            id: 5,
             title: 'mens',
             imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
             size: 'large',
-            id: 5,
+            linkURL: ''
+          },
+          {
+            id: 6,
+            title: 'Kids',
+            imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+            size: 'large',
             linkURL: ''
           }
         ]
