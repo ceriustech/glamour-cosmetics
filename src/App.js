face3e753488d7e309dc1912ceb86cb82d78c956
Bill <ceriustech@gmail.com>
2020-06-09 22:35:57 -0500

    Added header to component and set up routing to home, shop, and contact page. Added logo and styling to the body of the website.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'; 
+import Header from './components/header/header.component';
 
 import './App.css';
 
@@ -12,8 +13,11 @@ import './App.css';
 function App() {
   return (
     <div className="wrapper">
+      <Header />
+      <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
