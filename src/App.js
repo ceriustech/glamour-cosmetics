e47df018154dac7c9ef78ebd21ec849f6f174b4f
Bill <ceriustech@gmail.com>
2020-06-20 22:00:10 -0500

    Added useState hook to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { useState } from 'react'; 
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'; 
 import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
 import './App.css';
 
